256539cafc5b499ea717d0b4f0f3f0c573c2f15d
Codrut Ursache <ursache.codrut71@gmail.com>
2022-12-25 14:05:12 +0200

    telescope finished

--- lua/codrut/plugins/telescope.lua
+++ lua/codrut/plugins/telescope.lua
@@ -1,24 +1,30 @@
-local telescope_status, telescope = pcall(require, "telescope")
-if not telescope_status then
+local status, telescope = pcall(require, "telescope")
+if not status then
   return
 end
 
 local actions = require "telescope.actions"
 
-telesope.setup({
+-- Telescope options
+telescope.setup {
   defaults = {
+
     prompt_prefix = " ",
     selection_caret = "  ",
     path_display = { "smart" },
     file_ignore_patterns = { ".git/", "node_modules" },
-    
+
     mappings = {
       i = {
+        ["<A-q>"] = actions.close,
         ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previos,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<A-j>"] = actions.preview_scrolling_down,
+        ["<A-k>"] = actions.preview_scrolling_up,
       },
     },
-  }
-})
+  },
+}
 
-telescope.lead_extension("fzf")
+telescope.load_extension("fzf")
+
