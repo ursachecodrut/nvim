4676f57990bbb310659f19af7e4d9fb1c55e9179
Codrut Ursache <ursache.codrut71@gmail.com>
2024-06-14 01:01:29 +0300

    remove oil. fix icons

--- lua/codrut/plugins/telescope.lua
+++ lua/codrut/plugins/telescope.lua
@@ -12,8 +12,8 @@ return {
 
 		telescope.setup({
 			defaults = {
-				prompt_prefix = " ",
-				selection_caret = "  ",
+				prompt_prefix = " ",
+				selection_caret = "󰣉 ",
 				path_display = { "truncate" },
 				mappings = {
 					i = {
